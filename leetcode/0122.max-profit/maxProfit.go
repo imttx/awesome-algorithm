a7ea4a9f94ab47e475524d7f1b25a92687ad9cab
liyafeng01 <liyafeng01@baijiahulian.com>
2020-09-08 21:33:54 +0800

    leetcode

---
+++ leetcode/0122.max-profit/maxProfit.go
@@ -0,0 +1,18 @@
+package leetcode
+
+/*
+122. 买卖股票的最佳时机 II
+
+我们可以直接继续增加加数组的连续数字之间的差值，如果第二个数字大于第一个数字，我们获得的总和将是最大利润。
+这种方法将简化解决方案。
+*/
+func maxProfit(prices []int) int {
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+
+	return maxProfit
+}
